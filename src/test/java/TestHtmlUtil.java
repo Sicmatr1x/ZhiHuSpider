382cb9dfbcd7eea4fbacbe6aef0c0f62e5c5dc73
sicmatr1x <sicmatr1x@outlook.com>
2019-01-04 23:38:09 +0800

    add getOneAnswer function

--- src/test/java/TestHtmlUtil.java
+++ src/test/java/TestHtmlUtil.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import org.jsoup.nodes.Element;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,5 +22,24 @@ public class TestHtmlUtil {
 
     // Then
     Assert.assertNotNull(htmlUtil.getDoc());
+  }
+
+  @Test
+  public void testGetOneAnswer(){
+    // Given
+    String address = "https://www.zhihu.com/question/20584141/answer/82003627";
+    HtmlUtil htmlUtil = new HtmlUtil(address);
+
+    // When
+    try {
+      htmlUtil.getHtml();
+      Element answer = htmlUtil.getOneAnswer();
+      System.out.println(answer.html());
+
+      // Then
+      Assert.assertNotNull(answer);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
