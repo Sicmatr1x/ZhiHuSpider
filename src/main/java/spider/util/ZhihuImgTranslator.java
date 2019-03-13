fcd5418ff18f32030120e99fe0765c2f7bc1c11b
Joe <joe.guo@oocl.com>
2019-03-13 13:56:07 +0800

    fix can't get img src url address problem

--- src/main/java/spider/util/ZhihuImgTranslator.java
+++ src/main/java/spider/util/ZhihuImgTranslator.java
@@ -1,12 +1,25 @@
 package spider.util;
 
+import java.util.Iterator;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Element;
 
 public class ZhihuImgTranslator implements Translator{
 
   @Override
   public Element translate(Element imgElement) {
-    String srcAddress = imgElement.attr("data-original");
+    String srcAddress = null;
+    // smart get img src url
+    Attributes attrs = imgElement.attributes();
+    Iterator<Attribute> iterator = attrs.iterator();
+    while(iterator.hasNext()){
+      Attribute attr = iterator.next();
+      if(attr.getValue() != null && attr.getValue().contains("http")){
+        srcAddress = attr.getValue();
+      }
+    }
+
     String rawWidth = imgElement.attr("data-rawwidth");
     String rawHeight = imgElement.attr("data-rawheight");
     imgElement.attr("src", srcAddress);
